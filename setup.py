e599890666c873994ffff775d516c24e30ec7c00
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2025-02-12 15:13:52 +0800

    Fix : caching on the pages

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pupsis",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "selectolax",
+        "hishel",
+        "psutil",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pupsis=pupsis.cli:main",
+        ],
+    },
+    author="yam-1111",
+    description="unofficial python api wrapper for PUP SIS",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yam-1111/pupsis.py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
