32dd097a07b0030ed07b77b8ac9f144fc73fcc16
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2025-02-14 21:22:01 +0800

    modified the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pupsis",
+    name="pupsis.py",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -16,13 +16,14 @@ setup(
         ],
     },
     author="yam-1111",
-    description="unofficial python api wrapper for PUPSIS",
+    description="Unofficial Python API wrapper for PUPSIS",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yam-1111/pupsis.py",
+    license="HL3 License",
     classifiers=[
         "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Hippocratic License 3.0 (HL3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
